45f31cfeeb30b2b2c784e0c60ff9a1899a4dcc13
Deividas-BlueSkin <somecrazyp@gmail.com>
2023-10-02 13:42:04 +0300

    commit

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
-function create(HTMLelement, parent, classes, id, first) {
-    let element = document.createElement(HTMLelement)
-    if (parent) {
-        if (first) parent.prepend(element)
-        else parent.append(element)
-    }
-    else {
-        parent = document.body
-        if (first) parent.prepend(element)
-        else parent.append(element)
-    }
-    if (classes) {
-        element.className = classes
-    }
-    if (id) {
-        element.id = id
-    }
-    return element
-}
+import { create, deleteLastChild } from "./utils.js"
 
 init()
 function init() {
     const container = create('div', null, null, null, true)
-
     main(container)
 
 }
 
 function main(container) {
 
-    const dice = [4, 6, 8, 12, 20, 100]
+    const dice = [4, 6, 8, 10, 12, 20]
+    const diceX = [4, 6, 8, 10, 12]
     const diceBox = create('div', container, 'diceBox', null, true)
 
     makeDice(dice, diceBox)
 
-    customDie(diceBox)
+    customDice(diceBox)
+
+    multiDice(diceX, diceBox)
+}
+
+function fetchNum(value, resContainer, count = 1) {
+    fetch(`https://www.random.org/integers/?num=${count}&min=1&max=${value}&col=1&base=10&format=plain&rnd=new`
+    ).then(res => res.text()
+    ).then(function (data) {
+
+        let num = formatData(data, count)
+        printResult(resContainer, num)
+    })
+}
+
+function printResult(resContainer, num) {
+    let span = create('span', resContainer, 'res', null, true)
+    span.textContent = ` ${num}`
+    setTimeout(function () {
+        setTimeout(function () {
+            span.remove()
+        }, 27000)
+        span.style.color = 'lightgray'
+    }, 13000)
+}
+
+function formatData(data, count) {
+    if (count == 1) {
+        return parseInt(data)
+    } else {
+        let arr = []
+        arr = data.split('\n', count)
+        console.log(arr);
+        let num = 0
+        arr.forEach(function (item) {
+            num += parseInt(item)
+        })
+        return num
+    }
 }
 
 function makeDice(dice, diceBox) {
@@ -45,17 +65,23 @@ function makeDice(dice, diceBox) {
 
         let resContainer = create('span', dieContainer)
 
-        newDie.addEventListener('click', function (event) {
+        newDie.addEventListener('click', function () {
             fetchNum(newDie.value, resContainer)
+            deleteLastChild(resContainer, 13)
         })
     })
 }
 
-function customDie(diceBox) {
+function doubleDice() {
+    
+}
+
+function customDice(diceBox) {
     let dieContainer = create('div', diceBox, 'dieContainer')
     let input = create('input', dieContainer)
     input.type = 'number'
     input.min = 2
+    input.max = 1000
     input.value = 13
 
     let die = create('button', dieContainer, 'die')
@@ -66,6 +92,9 @@ function customDie(diceBox) {
         if (input.value < 2) {
             input.value = 2
         }
+        else if (input.value > 1000) {
+            input.value = 1000
+        }
 
         die.textContent = `d${input.value}`
         die.value = input.value
@@ -75,28 +104,49 @@ function customDie(diceBox) {
 
     die.addEventListener('click', function () {
         fetchNum(die.value, resContainer)
+        deleteLastChild(resContainer, 13)
     })
 }
 
-function fetchNum(value, resContainer) {
-    fetch(`https://www.random.org/integers/?num=1&min=1&max=${value}&col=1&base=10&format=plain&rnd=new`
-    ).then(res => res.json()
-    ).then(data => {
-        let span = create('span', resContainer, 'res', null, true)
-        span.textContent = ` ${data}`
-        setTimeout(function () {
-            setTimeout(function () {
-                span.remove()
-            }, 27000)
-            span.style.color = 'lightgray'
-        }, 13000)
+function multiDice(dice, diceBox) {
+    const counter = create('div', diceBox)
+    const minus = create('button', counter)
+    const count = create('span', counter)
+    const plus = create('button', counter)
 
-        deleteLastChild(resContainer)
+    plus.textContent = '+'
+    minus.textContent = '-'
+    count.textContent = 2
+    count.style = `padding: 10px`
+
+    minus.addEventListener('click', () => {
+        let num = count.textContent
+        if (num > 1) {
+            num--
+        }
+        count.textContent = num
+    })
+    plus.addEventListener('click', () => {
+        let num = count.textContent
+        if (num < 99) {
+            num++
+        }
+        count.textContent = num
+    })
+
+    dice.forEach(function (die) {
+        let dieContainer = create('div', diceBox, 'dieContainer')
+        let newDie = create('button', dieContainer, 'die')
+        newDie.textContent = `d${die}`
+        newDie.value = die
+
+        let resContainer = create('span', dieContainer)
+
+        newDie.addEventListener('click', function () {
+            fetchNum(newDie.value, resContainer, count.textContent)
+            deleteLastChild(resContainer, 13)
+        })
     })
 }
 
-function deleteLastChild(parent) {
-    if (parent.childElementCount > 13) {
-        parent.lastChild.remove()
-    }
-}
+
---
+++ utils.js
@@ -0,0 +1,25 @@
+export function create(HTMLelement, parent, classes, id, first) {
+    let element = document.createElement(HTMLelement)
+    if (parent) {
+        if (first) parent.prepend(element)
+        else parent.append(element)
+    }
+    else {
+        parent = document.body
+        if (first) parent.prepend(element)
+        else parent.append(element)
+    }
+    if (classes) {
+        element.className = classes
+    }
+    if (id) {
+        element.id = id
+    }
+    return element
+}
+
+export function deleteLastChild(parent, count) {
+    if (parent.childElementCount > count) {
+        parent.lastChild.remove()
+    }
+}
